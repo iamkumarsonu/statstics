bdeb7170d392c8441f1f842f8d0b90e2a35a3365
iamkumarsonu <sonukumarcuj007@gmail.com>
2023-10-18 00:04:52 +0530

    Added Union Function

---
+++ union-intersection.py
@@ -0,0 +1,27 @@
+a = (1,2,3,4,5,5,6)
+class unionSet():
+    def __init__(self,a,b):
+        self.a = a
+        self.b = b
+    def intersectionValue(self):
+        returnTuple = ()
+        for item in self.a:
+            if item  in self.b:
+                returnTuple += (item,)
+        return returnTuple
+    def unionValues(self):
+        returnTuple = ()
+        for item in self.a:
+            if item not in returnTuple:
+                returnTuple +=(item,)
+        for item in self.b:
+            if item not in returnTuple:
+                returnTuple +=(item,)      
+        return returnTuple
+        
+                
+a = (1,2,3,4,5,5,6)
+b = (1,2,3,7,8)   
+unionSet(a,b).intersectionValue()
+unionSet(a,b).unionValues()
+
